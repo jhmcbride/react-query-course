be028ef39582c3150edcd93b9594fd1e0cbc2bee
Jay <jhmcbride@gmail.com>
2023-03-17 13:53:19 +1000

    feat: add search to IssuesList

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { IssueItem } from "./IssueItem";
 
 export default function IssuesList({ labels, status }) {
@@ -10,13 +11,49 @@ export default function IssuesList({ labels, status }) {
     );
   });
 
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchQuery = useQuery(
+    ["issues", "search", searchValue],
+    () =>
+      fetch(`/api/search/issues?q=${searchValue}`).then((res) => res.json()),
+    {
+      enabled: searchValue.length > 0,
+    }
+  );
+  console.log({ searchQuery });
+
+  const isSearch = !(
+    searchQuery.fetchStatus === "idle" && searchQuery.isLoading === true
+  );
   return (
     <div>
-      <h2>Issues List</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          // @ts-ignore
+          setSearchValue(event.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search">Search Issues</label>
+        <input
+          type="text"
+          placeholder="Search"
+          id="search"
+          name="search"
+          onChange={(event) => {
+            if (event.target.value.length === 0) {
+              setSearchValue("");
+            }
+          }}
+        />
+      </form>
 
+      <h2>{isSearch ? "Search Results" : "Issues List"}</h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
-      ) : (
+      ) : searchQuery.fetchStatus === "idle" &&
+        searchQuery.isLoading === true ? (
         <ul className="issues-list">
           {issuesQuery.data.map((issue) => (
             <IssueItem
@@ -32,6 +69,21 @@ export default function IssuesList({ labels, status }) {
             />
           ))}
         </ul>
+      ) : (
+        <>
+          {searchQuery.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <p>{searchQuery.data.count} Results</p>
+              <ul className="issues-list">
+                {searchQuery.data.items.map((issue) => (
+                  <IssueItem {...issue} />
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       )}
     </div>
   );
